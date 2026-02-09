c5a333cf9877235d1a905e028f0eedf272c6b414
tsjerkvdm <tsjerk.van.der.meij@gmail.com>
2026-02-09 09:33:11 +0000

    1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.content.part} {props.content.excercises}
+    </p>)
+}
+
+const Total = (props) => {
+
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const content =  [
+    { part: part1, excercises: exercises1 },
+    { part: part2, excercises: exercises2 },
+    { part: part3, excercises: exercises3 },
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content[0]} />
+      <Content content={content[1]} />
+      <Content content={content[2]} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
